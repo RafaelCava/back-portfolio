3aeec7254df6a5bc9840418095dc42308ffc8c59
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-07-29 11:23:08 -0300

    feat(feature/swagger): implements swagger openApi

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Api de Mensagens')
+    .setDescription('API de teste para envio de mensagens')
+    .setVersion('1.0')
+    .setContact(
+      'Rafael',
+      'https://github.com/RafaelCava',
+      'rafael.cavalcante@tallos.com.br',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
