72aa80aded68835377046877d9133458fb314c76
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-07-31 11:47:10 -0300

    feat(feature/websockets): implements socket in the api

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { UserService } from './user/user.service';
 import { MessageService } from './message/message.service';
 import { UserController } from './user/user.controller';
 import { MessageController } from './message/message.controller';
+import { WebsocketService } from './websocket/websocket.service';
 
 @Module({
   imports: [],
   controllers: [UserController, MessageController],
-  providers: [AppService, PrismaService, UserService, MessageService],
+  providers: [
+    AppService,
+    PrismaService,
+    UserService,
+    MessageService,
+    WebsocketService,
+  ],
 })
 export class AppModule {}
