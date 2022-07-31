e3c2dc4306d2d79e0f9872b41d428235edbe254e
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-07-31 16:03:37 -0300

    feat(husky): insert husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserService } from './user/user.service';
 import { MessageService } from './message/message.service';
@@ -10,12 +9,6 @@ import { WebsocketService } from './websocket/websocket.service';
 @Module({
   imports: [],
   controllers: [UserController, MessageController],
-  providers: [
-    AppService,
-    PrismaService,
-    UserService,
-    MessageService,
-    WebsocketService,
-  ],
+  providers: [PrismaService, UserService, MessageService, WebsocketService],
 })
 export class AppModule {}
